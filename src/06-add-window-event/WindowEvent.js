ab84bf90dd3ce058d225a14d675443ac8883efcb
doguhanciftci <doguhan.ciftci@maximintegrated.com>
2022-11-24 18:00:26 +0300

    01_06 My Solution

--- src/06-add-window-event/WindowEvent.js
+++ src/06-add-window-event/WindowEvent.js
@@ -1,4 +1,15 @@
+import { useEffect } from "react"
+
+const alertMessage = () => alert("'Double Clicked!")
+
 export default function WindowEvent () {
+
+  useEffect(() => {
+      window.addEventListener('dblclick', alertMessage);
+
+      return () => window.removeEventListener('dblclick', alertMessage);
+  }, []);
+
   return (
     <h2>Window event active</h2>
   )
