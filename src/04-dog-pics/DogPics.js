0d609807ef1e5d39894b62a93c946894881fa4fd
doguhanciftci <doguhan.ciftci@maximintegrated.com>
2022-11-24 17:17:57 +0300

    01_04 My Solution

--- src/04-dog-pics/DogPics.js
+++ src/04-dog-pics/DogPics.js
@@ -1,9 +1,21 @@
+import { useState } from 'react';
+
 export default function DogPics () {
   // API: https://dog.ceo/dog-api/
+
+  const [source, setSource] = useState('https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg');
+
+  const handleClick = () => {
+
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(res => res.json())
+      .then(data => setSource(data.message))
+  }
+
   return (
     <div className='dog-pics'>
-      <img src='https://images.dog.ceo/breeds/spaniel-cocker/n02102318_4172.jpg' />
-      <button>🐶</button>
+      <img alt='random dog images' src={source} />
+      <button onClick={handleClick}>🐶</button>
     </div>
   )
 }
